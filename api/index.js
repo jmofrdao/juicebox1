57f5dfff66f6bde6fd9e612dd07958d816dfc2cc
Gabriel <gabrieljch929@gmail.com>
2022-07-20 10:57:35 -0500

    Post Request

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const jwt = require('jsonwebtoken');
 const { getUserById } = require('../db');
 const { JWT_SECRET } = process.env;
 
+
+  
+
 // set `req.user` if possible
 apiRouter.use(async (req, res, next) => {
   const prefix = 'Bearer ';
@@ -31,6 +34,13 @@ apiRouter.use(async (req, res, next) => {
     });
   }
 });
+apiRouter.use((req, res, next) => {
+    if (req.user) {
+      console.log("User is set:", req.user);
+    }
+  
+    next();
+  });
 
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
--- api/users.js
+++ api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const usersRouter = express.Router();
-const { getAllUsers } = require('../db');
+const { getAllUsers, getUserByUsername, createUser } = require('../db');
+const jwt = require('jsonwebtoken'); 
+const {JWT_SECRET} = process.env
+
 
 
 usersRouter.use((req, res, next) => {
@@ -31,8 +34,14 @@ usersRouter.post('/login', async (req, res, next) => {
     const user = await getUserByUsername(username);
 
     if (user && user.password == password) {
+     const token = jwt.sign({
+      id: user.id,
+      username
+     }, JWT_SECRET, {
+      expiresIn: "2w"
+     })
       // create token & return to user
-      res.send({ message: "you're logged in!" });
+      res.send({ message: "you're logged in!", token });
     } else {
       next({ 
         name: 'IncorrectCredentialsError', 
@@ -43,6 +52,41 @@ usersRouter.post('/login', async (req, res, next) => {
     console.log(error);
     next(error);
   }
+});
+usersRouter.post('/register', async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: 'UserExistsError',
+        message: 'A user by that username already exists'
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign({ 
+      id: user.id, 
+      username
+    },  JWT_SECRET, {
+      expiresIn: "2w"
+    });
+
+    res.send({ 
+      message: "thank you for signing up",
+      token 
+    });
+  } catch ({ name, message }) {
+    next({ name, message })
+  } 
 });
 
 module.exports = usersRouter;
