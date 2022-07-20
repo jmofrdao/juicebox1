3985387d2276f8d2988e05a9cd9ce18aeacbbd55
Gabriel <gabrieljch929@gmail.com>
2022-07-20 11:10:39 -0500

    Writting Post

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const postsRouter = express.Router();
 const { getAllPosts } = require('../db');
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  res.send({ message: 'under construction' });
+});
+
 
 postsRouter.use((req, res, next) => {
     console.log("A request is being made to /posts");
