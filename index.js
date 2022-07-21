50c50b0cb0ee2d351941579ada2ff6b3b9d011f5
Gabriel <gabrieljch929@gmail.com>
2022-07-21 13:30:18 -0500

    Last commit

--- index.js
+++ index.js
@@ -10,33 +10,21 @@ server.use(morgan('dev'));
 
 server.use(express.json())
 
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
 
-server.get('/background/:color', (req, res, next) => {
-  res.send(`
-    <body style="background: ${ req.params.color };">
-      <h1>Hello World</h1>
-    </body>
-  `);
-});
 
-server.get('/add/:first/to/:second', (req, res, next) => {
-  res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
-    Number(req.params.first) + Number(req.params.second)
-   }</h1>`);
-});
 
 
 server.use((req, res, next) => {
   console.log("<____Body Logger START____>");
   console.log(req.body);
   console.log("<_____Body Logger END_____>");
-
+  
   next();
 });
 
 
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
 
 const {PORT = 3000}=process.env;
 server.listen(PORT, () => {
